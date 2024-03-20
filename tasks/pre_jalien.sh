aa94569a66bcf4f9c9d60a8453f82c24f75923a1
Malith Dilshan <79596630+Malith-19@users.noreply.github.com>
2024-03-20 23:45:58 +0530

    Add the pull function to script. (#8)
    * Add the pull function to code.
    
    * Update the config example with base dir.
    
    * Remove the unneccesary codes.
    
    * Add base dir to example.
    
    * Removed the sample.

--- tasks/pre_jalien.sh
+++ tasks/pre_jalien.sh
@@ -12,8 +12,33 @@ clone_if_not_exists() {
         echo "$git_repo cloned to $dir_name."
     else
         echo "Directory $dir_name already exists, skipping cloning."
+        pull_new_changes "$dir_name"
+    fi
+
+}
+
+pull_new_changes() {
+    local dir_name="$1"
+    cd "$dir_name" || return
+
+    # Fetch changes from all remote branches
+    git fetch --all
+
+    # Check if there are any changes to be pulled
+    if [ $(git rev-list HEAD...@{u} --count) -gt 0 ]; then
+
+        # Changes exist, ask the user if they want to pull
+        read -p "There are new changes. Do you want to pull them? (y/n): " response
+        if [[ $response =~ ^[Yy]$ ]]; then
+            git pull --all
+        else
+            echo "Changes not pulled."
+        fi
+    else
+        echo "No new changes to pull."
     fi
 }
+
 
 
 mkdir -p "$BASE_DIR" && cd "$BASE_DIR" || exit 1
